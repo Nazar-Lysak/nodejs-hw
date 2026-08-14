d05f530d1d571db0af012aa28f1abe1faee2b724
Nazarii Lysak <nazarii.lysak@ua.nestle.com>
2026-08-14 13:41:08 +0300

    remove useless spaces

--- src/server.js
+++ src/server.js
@@ -8,7 +8,6 @@ import { logger } from './middleware/logger.js';
 import notesRoutes from './routes/notesRoutes.js';
 import studentsRoutes from './routes/studentsRoutes.js';
 
-
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 await connectMongoDB();
@@ -16,7 +15,6 @@ await connectMongoDB();
 app.use(cors());
 app.use(logger);
 app.use(express.json());
-
 
 app.use(notesRoutes);
 app.use(studentsRoutes);
